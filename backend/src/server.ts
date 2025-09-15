fa4146749e5325cb207de1886ff29ddc7e1ae35a
Phuc <119803257+pauln17@users.noreply.github.com>
2025-09-15 19:43:22 -0400

    basic cors setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 
 const prisma = new PrismaClient()
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware (optional)
+const allowedOrigins = [
+    'http://localhost:4000'
+];
+
+app.use(
+    cors({
+        origin: allowedOrigins,
+    })
+);
 app.use(express.json());
+
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
