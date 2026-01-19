e95f1049dd4cc84911971e135c31d4638796b3a3
Phuc <119803257+pauln17@users.noreply.github.com>
2026-01-19 12:18:11 -0500

    clean up

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link'
+
+
 export default function Home() {
   return (
-    <div></div>
+    <nav className=" text-white px-6 py-4 border-b border-gray-800">
+      <div className="flex max-w-6xl">
+        <Link href="/" className="text-3xl">
+          Schema Explorer
+        </Link>
+      </div>
+
+    </nav>
   );
 }
